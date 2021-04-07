a9297397f261462c1df13bb4af57b71c71fbcd99
erik <tuananh1421999@gmail.com>
2021-04-07 14:54:11 +0700

    restore requirements to install swagger

--- center/turtorial1/urls.py
+++ center/turtorial1/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title="my swagger project")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls'))
+    path('home/', include('home.urls')),
+    path('',schema_view)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

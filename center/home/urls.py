8255c7717e5fc268927f057be0e6fbe33e135130
erik <tuananh1421999@gmail.com>
2021-04-08 01:33:31 +0700

    them hai api : add vaf get model

--- center/home/urls.py
+++ center/home/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('<str:ver>', views.get_model_detail),
     path('<str:ver>/add', views.add_model_to_db),
     path('home/test_swagger', views.test_swagger.as_view()),
+    path('home/get_model/<str:ver>', views.get_model_by_version.as_view()),
+    path('home/add_model_manual/<str:ver>/<str:url>', views.add_model_manual.as_view()),
 ]
 

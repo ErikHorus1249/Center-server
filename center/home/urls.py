23d4f438271ec58aed3a926e20c887610698ef16
erik <tuananh1421999@gmail.com>
2021-04-08 10:03:56 +0700

    fix missing

--- center/home/urls.py
+++ center/home/urls.py
@@ -4,12 +4,8 @@ from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-# router.register(r'home', views.HomeViewSet)
 
 urlpatterns = [
-    path('',views.home),
-    path('<str:ver>', views.get_model_detail),
-    path('<str:ver>/add', views.add_model_to_db),
     path('home/test_swagger', views.test_swagger.as_view()),
     path('home/get_model/<str:ver>', views.get_model_by_version.as_view()),
     path('home/add_model_manual/<str:ver>/<str:url>', views.add_model_manual.as_view()),

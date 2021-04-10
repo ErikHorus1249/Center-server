7ab4eb140cf8a32561d118a0f3cf0082dfe4f2aa
erik <tuananh1421999@gmail.com>
2021-04-10 12:03:02 +0700

    gitignore media folder

--- center/steppenwolf/serializers.py
+++ center/steppenwolf/serializers.py
@@ -4,4 +4,4 @@ from .models import File
 class FileSerializer(serializers.ModelSerializer):
   class Meta():
     model = File
-    fields = ('file', 'remark', 'timestamp')
+    fields = ('file', 'version', 'timestamp')
